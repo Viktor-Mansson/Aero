112e00317da0d1689c43ac6f9ffc056bfc7dc57f
JohannesNivala <norenjojo02@gmail.com>
2026-05-08 12:49:10 +0200

    Fixed heart collisions to be more accurate

--- app/src/main/java/com/aero/android/aero/HeartAnimator.java
+++ app/src/main/java/com/aero/android/aero/HeartAnimator.java
@@ -38,7 +38,7 @@ public class HeartAnimator {
 
     public boolean isCollision(View v1) {
         Rect rect1 = getCustomHitbox(v1, 40, 40);
-        Rect rect2 = getCustomHitbox(heart, 50, 50);
+        Rect rect2 = getCustomHitbox(heart, 20, 20);
 
         if (Rect.intersects(rect1, rect2)) {
             resetHeart();
